a5384aff0b19b111610936768a691daf08baa183
PROBULL86 <probull86@gmail.com>
2023-06-12 20:55:30 -0300

    Vista Sobre Nosotros (No terminada)
    Se empezó a realizar la vista Sobre Nosotros, pero aún no está finalizada.
    Faltante: Objetivos.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -102,16 +102,17 @@
         </div>
 
         <div class="flex items-center bg-grayCanvas ">
-            <div class="text-left py-2 pl-60 mr-10">
+            <div class="text-left py-3 pl-60 mr-10">
                 <h1 class="text-4xl font-bold mb-3">Sobre Nosotros</h1>
                 <p class="text-lg text-grayP">Canvas es una tienda especializada en productos y servicios para mascotas.
                     La
                     cual se centra en brindar una experiencia diferente de compra, ofreciendo productos y servicios de
                     alta
                     calidad que buscan cumplir con las necesidades de sus cachorros</p>
-                <button class="mt-6 bg-secondary text-white font-bold py-2 px-6 rounded">Sobre nosotros</button>
+                <button class="mt-6 bg-secondary text-white font-bold py-3 px-6 rounded-md">Sobre nosotros</button>
             </div>
             <img src="{{ asset('img/perroygato.jpg') }}" class="w-110 h-80 object-cover ml-10 pt-8 pb-8">
         </div>
     </div>
+    <x-footercanvas/>
 </x-app-layout>
