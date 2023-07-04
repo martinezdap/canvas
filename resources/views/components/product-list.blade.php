78f64d1f930c68f998ed3fbffc57756d07b97704
Lucas260602 <lucass.67@outlook.es>
2023-07-04 18:27:39 -0300

    sprint 6 funcionamiento del buscador
    Se agrego la funcionalidad al buscador de productos.

---
+++ resources/views/components/product-list.blade.php
@@ -0,0 +1,24 @@
+@props(['product'])
+<li class="bg-white rounded-sm mb-4">
+    <article class="flex">
+        <figure>
+            <img class="h-48 w-56 objet-cover object-center"
+                src="{{ Storage::url($product->images->first()->url) }}" alt="">
+        </figure>
+        <div class="flex-1 py-4 px-6 flex flex-col justify-between">
+            <div class="flex justify-between">
+                <div>
+                    <h1 class="text-xl font-semibold text-blackCanvas">{{ $product->name }}
+                    </h1>
+                    <p class="font-semibold text-grayP text-xl">$ {{ $product->price }}</p>
+                </div>
+
+            </div>
+            <div class="">
+                <x-button>
+                    Mas informacion
+                </x-button>
+            </div>
+        </div>
+    </article>
+</li>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\SearchController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CategoryController;
 
@@ -9,6 +10,7 @@ Route::get('/', WelcomeController::class);
 
 Route::get('categories/{category}', [CategoryController::class, 'show'])->name('categories.show');
 
+route::get('search', SearchController::class)->name('search');
 Route::get('products/{product}', [ProductController::class, 'show'])->name('products.show');
 
 Route::get('sobrenosotros', function(){
